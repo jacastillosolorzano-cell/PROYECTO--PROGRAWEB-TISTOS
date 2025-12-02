0f45b5bdd970ec9b677bfb99d98a087c1245eb09
JOTAC1907 <jcs27001@gmail.com>
2025-12-02 18:14:08 -0500

    despliegue2

--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { SaldoProvider } from './contexts/SaldoContext'; 
 
 
@@ -32,7 +32,7 @@ createRoot(document.getElementById("root")!).render(
         <SaldoProvider>  
           <Toaster />
           <Sonner />
-          <HashRouter basename = "/PROYECTO--PROGRAWEB-TISTOS/tree/main/FRONTEND/">
+          <HashRouter basename={import.meta.env.BASE_URL}>
             <Suspense fallback={<div className="p-4">Cargando...</div>}>
               <Routes>
                 <Route path="/" element={<Login />} />
@@ -52,7 +52,6 @@ createRoot(document.getElementById("root")!).render(
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </Suspense>
-            <Toaster />
           </HashRouter>
         </SaldoProvider>
       </TooltipProvider>
